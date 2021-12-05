c3b3ffb76aecdbcc178d04424cf83094f9d732a1
Noraluk <noraluk.kn@gmail.com>
2021-12-05 17:12:45 +0700

    :white_check_mark: update pump service unit test

--- api/services/pump/pump_test.go
+++ api/services/pump/pump_test.go
@@ -1,9 +1,11 @@
 package pumpservice
 
 import (
+	"errors"
 	pumpmodel "plant-system-api/api/models/pump"
 	mockPumprepoitf "plant-system-api/api/repositories/pump/mock"
 	mockConfig "plant-system-api/config/mock"
+	"reflect"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -77,7 +79,7 @@ func Test_pumpService_ActivePump(t *testing.T) {
 			},
 			args: args{
 				pump: &pumpmodel.Pump{
-					ID:   pumpID,
+					ID:       pumpID,
 					IsActive: true,
 				},
 			},
@@ -93,6 +95,139 @@ func Test_pumpService_ActivePump(t *testing.T) {
 			}
 			if err := s.ActivePump(tt.args.pump); (err != nil) != tt.wantErr {
 				t.Errorf("pumpService.ActivePump() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_pumpService_AskPump(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	const (
+		pumpID = 1
+	)
+
+	var (
+		fooErr = errors.New("foo")
+	)
+
+	type fields struct {
+		pumpRepository         *mockPumprepoitf.MockPumpRepository
+		pumpRepositoryBehavior func(m *mockPumprepoitf.MockPumpRepository)
+	}
+	type args struct {
+		pump *pumpmodel.Pump
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "success",
+			fields: fields{
+				pumpRepository: mockPumprepoitf.NewMockPumpRepository(ctrl),
+				pumpRepositoryBehavior: func(m *mockPumprepoitf.MockPumpRepository) {
+					m.EXPECT().IsPumpWorking(gomock.Any()).Return(nil)
+					m.EXPECT().AskPump(gomock.Any()).Return(nil)
+				},
+			},
+			args: args{
+				pump: &pumpmodel.Pump{
+					ID:       pumpID,
+					IsActive: true,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "error getting is pump working",
+			fields: fields{
+				pumpRepository: mockPumprepoitf.NewMockPumpRepository(ctrl),
+				pumpRepositoryBehavior: func(m *mockPumprepoitf.MockPumpRepository) {
+					m.EXPECT().IsPumpWorking(gomock.Any()).Return(fooErr)
+				},
+			},
+			args: args{
+				pump: &pumpmodel.Pump{
+					ID:       pumpID,
+					IsActive: true,
+				},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		tt.fields.pumpRepositoryBehavior(tt.fields.pumpRepository)
+
+		t.Run(tt.name, func(t *testing.T) {
+			s := &pumpService{
+				pumpRepository: tt.fields.pumpRepository,
+			}
+			if err := s.AskPump(tt.args.pump); (err != nil) != tt.wantErr {
+				t.Errorf("pumpService.AskPump() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_pumpService_GetPump(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	const (
+		pumpID = 1
+	)
+
+	var (
+		expected = &pumpmodel.Pump{ID: pumpID}
+	)
+
+	type fields struct {
+		pumpRepository         *mockPumprepoitf.MockPumpRepository
+		pumpRepositoryBehavior func(m *mockPumprepoitf.MockPumpRepository)
+	}
+	type args struct {
+		id int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *pumpmodel.Pump
+		wantErr bool
+	}{
+		{
+			name: "success",
+			fields: fields{
+				pumpRepository: mockPumprepoitf.NewMockPumpRepository(ctrl),
+				pumpRepositoryBehavior: func(m *mockPumprepoitf.MockPumpRepository) {
+					m.EXPECT().GetPump(gomock.Any()).Return(expected, nil)
+				},
+			},
+			args: args{
+				id: pumpID,
+			},
+			want:    expected,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		tt.fields.pumpRepositoryBehavior(tt.fields.pumpRepository)
+
+		t.Run(tt.name, func(t *testing.T) {
+			s := &pumpService{
+				pumpRepository: tt.fields.pumpRepository,
+			}
+			got, err := s.GetPump(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("pumpService.GetPump() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("pumpService.GetPump() = %v, want %v", got, tt.want)
 			}
 		})
 	}
