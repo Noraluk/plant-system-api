e0eacd33c52ed2dfae55a580deaa55faeedaa2f0
Noraluk <noraluk.kn@gmail.com>
2022-01-29 12:59:51 +0700

    deploy : with routes

--- main.go
+++ main.go
@@ -2,28 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"plant-system-api/api/routes"
+	"plant-system-api/config"
 
+	firebase "firebase.google.com/go"
 	"github.com/labstack/echo/v4"
+	"google.golang.org/api/option"
 )
 
 func main() {
 	e := echo.New()
 
-	// conf := &firebase.Config{
-	// 	DatabaseURL: os.Getenv("FIREBASE_DB_URL"),
-	// }
+	conf := &firebase.Config{
+		DatabaseURL: os.Getenv("FIREBASE_DB_URL"),
+	}
 
-	// opt := option.WithCredentialsFile(os.Getenv("FIREBASE_CREDENTIALS_FILE_PATH"))
-	// firebaseClient, err := config.NewFirebaseClient(conf, opt)
-	// if err != nil {
-	// 	// e.Logger.Fatal(err)
-	// 	log.Println(err)
-	// }
+	opt := option.WithCredentialsFile(os.Getenv("FIREBASE_CREDENTIALS_FILE_PATH"))
+	firebaseClient, err := config.NewFirebaseClient(conf, opt)
+	if err != nil {
+		// e.Logger.Fatal(err)
+		log.Println(err)
+	}
 
-	// route := routes.NewRoute(e, config.NewClient(firebaseClient))
-	// route.Init()
+	route := routes.NewRoute(e, config.NewClient(firebaseClient))
+	route.Init()
 
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))))
-	// fmt.Println("START")
 }
